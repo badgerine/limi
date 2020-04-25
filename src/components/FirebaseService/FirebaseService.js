fd1b27e259e9495ee091a1af537e1c535f5a996f
sibusiso matsini <sbu.matsini@gmail.com>
2020-04-25 13:46:24 +0200

    add firebase

---
+++ src/components/FirebaseService/FirebaseService.js
@@ -0,0 +1,16 @@
+import * as firebaseService from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyD2uIp8ew6goBwgSzB3QtIVT78ohMNZKDc",
+  authDomain: "limi-46e7d.firebaseapp.com",
+  databaseURL: "https://limi-46e7d.firebaseio.com",
+  projectId: "limi-46e7d",
+  storageBucket: "limi-46e7d.appspot.com",
+  messagingSenderId: "213593642858",
+  appId: "1:213593642858:web:00a9fcbe718d89396eb713",
+  measurementId: "G-50JTM0J0N2"
+};
+
+firebaseService.initializeApp(config);
+
+export default firebaseService;
