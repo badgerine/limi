dc9cc74822eae0fce3dcc1bf1fe7306e0b0f5e43
sibusiso matsini <sbu.matsini@gmail.com>
2020-07-22 21:42:20 +0200

    initial facelift

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
-import logo from './logo.svg';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import BackOffice from './components/BackOffice/BackOffice';
+import StoryRoll from './components/StoryRoll/StoryRoll';
+import StoryDeck from './containers/StoryDeck/StoryDeck';
 import './App.css';
 import Layout from './containers/Layout/Layout';
 
-function App() {
+const App = (props) => {
+
+  const routes = (
+    <Switch>
+      <Route path='/story-roll' component={StoryRoll} />
+      <Route path='/story-deck' component={StoryDeck} />
+      <Route path='/backoffice' component={BackOffice} />
+      <Redirect from='/' exact to='/story-deck' />
+    </Switch>
+  );
+
   return (
     <BrowserRouter>
-      <Layout />
+      <Layout>{routes}</Layout>
     </BrowserRouter>
   );
 }
