e6cb4e0198bd10ab3c165240704cf1d7b9d9e0d1
sibusiso matsini <sbu.matsini@gmail.com>
2020-07-30 08:41:56 +0200

    adjust sign in spacing

--- src/components/Authentication/Authentication.js
+++ src/components/Authentication/Authentication.js
@@ -15,7 +15,7 @@ import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(12),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
