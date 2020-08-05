4d5c7d5011b57c4c82b41d12b738de1e84c9da8a
sibusiso matsini <sbu.matsini@gmail.com>
2020-08-05 14:59:22 +0200

    story-roll uses material-ui components

--- src/components/StoryRoll/StoryRoll.js
+++ src/components/StoryRoll/StoryRoll.js
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import firebase from '../FirebaseService/FirebaseService';
-import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Container from '@material-ui/core/Container';
-import { Grid, Box } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import ScheduleIcon from '@material-ui/icons/Schedule';
 
 import useStyles from '../../containers/Layout/styles';
 
@@ -36,34 +30,17 @@ export default (props) => {
   const classes = useStyles();
 
   console.log('videoUrl: ',videoUrl);
-  const video = <video id='onScreen' src={videoUrl} controls playsInline type="video/mp4" style={{ borderRadius: '5px' }} />
+  const video = <video id='onScreen' src={videoUrl} controls playsInline type="video/mp4" style={{ borderRadius: '5px'}} />
 
   return (
-    // <div style={{ backgroundColor: '#000000', height: '100vh' }}>
-    //   <div style={{
-    //     marginLeft: 'auto',
-    //     marginRight: 'auto',
-    //     width: '90%',
-    //     height: '90%',
-    //     paddingTop: '5%',
-    //     paddingBottom: '5%',
-    //   }}>
-    //     {video}
-    //   </div>
-    // </div>
-
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="lg" style={{ backgroundColor: '#000000', height: '100vh' }}>
       <CssBaseline />
       <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
+        <Typography align='center' component="h1" variant="h5">
           Story name
         </Typography>
-        <Card className={classes.card}>
-          <CardMedia
-            className={classes.cardMedia}
-            src={videoUrl}
-            title={'Story Title'}
-          />
+        <Card className={classes.card} style={{ backgroundColor: '#000000'}}>
+          {video}
         </Card>
       </div>
     </Container>
